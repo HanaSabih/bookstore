27ec1ab944389c9c91654a205e5ec6b3d2ac9626
hana <hanasabih@gmail.com>
2023-02-27 09:57:07 +0200

    fix errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import categoriesReducer from './categories/categories';
-import booksReducer from './books/books';
+import categoriesReducer from './categories/categoriesSlice';
+import booksReducer from './books/booksSlice';
 
 const store = configureStore({
   reducer: {
