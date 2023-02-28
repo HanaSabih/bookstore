d708209479de98b27b0ec364fc9dd40aa77d6ae5
hana <hanasabih@gmail.com>
2023-02-28 11:35:31 +0200

    redux-in-react

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categoriesSlice';
 
-const Categories = () => (
-  <section className="categories-container">
-    <button type="button" className="check-btn">
-      Check Status
-    </button>
-  </section>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  const handleStatusCheck = () => {
+    dispatch(checkStatus());
+  };
+  return (
+    <div className="categories-page">
+      <button
+        className="status-btn btn"
+        type="button"
+        onClick={handleStatusCheck}
+      >
+        CHECK STATUS
+      </button>
+    </div>
+  );
+};
 
 export default Categories;
